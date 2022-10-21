e57e7f877dc89c68f25e008bb3050349cfe5a04d
Matej Pavlicek <tikinang@gmail.com>
2022-10-21 12:21:28 +0200

    dotnet improved

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var x = Environment.GetEnvironmentVariable("MY_X_VAR");
 app.MapGet("/", () => "hello_world" + x);
+app.MapGet("/version", () => Environment.Version.ToString());
+app.MapGet("/var", () => Environment.GetEnvironmentVariable("MY_X_VAR"));
 
 app.Run();
